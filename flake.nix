eb14f923c642ee1b49ceccbd5aa5547ecbca6eb2
Florent Charpentier <florent.charpentier@epitech.eu>
2024-10-28 11:09:45 +0100

    :snowflake: Added Nix flake with cargo 1.75

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = '':
+    Gomoku "l'eau ça rouille" Flake
+  '';
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-cargo-1-75.url = "github:NixOS/nixpkgs/9a9dae8f6319600fa9aebde37f340975cab4b8c0";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-cargo-1-75, ... }:
+    let
+      system = "x86_64-linux";
+    in {
+      devShells."${system}".default = let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        cargo = (import nixpkgs-cargo-1-75 { inherit system; }).cargo;
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractiveFHS
+
+          cargo
+        ];
+
+        shellHook = ''
+          echo Custom Cargo nix-shell !
+        '';
+      };
+    };
+}
