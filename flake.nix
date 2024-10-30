f817fb6dcbaafdbc2bdc4d3d25427a27de985769
Florent Charpentier <florent.charpentier@epitech.eu>
2024-10-30 23:26:58 +0100

    :snowflake: Added rustc to nix dev shell

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-cargo-1-75.url = "github:NixOS/nixpkgs/9a9dae8f6319600fa9aebde37f340975cab4b8c0";
+    nixpkgs-rust-1-75.url = "github:NixOS/nixpkgs/9a9dae8f6319600fa9aebde37f340975cab4b8c0";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-cargo-1-75, ... }:
+  outputs = { self, nixpkgs, nixpkgs-rust-1-75, ... }:
     let
       system = "x86_64-linux";
     in {
@@ -16,14 +16,17 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        cargo = (import nixpkgs-cargo-1-75 { inherit system; }).cargo;
+        rust1-75 = import nixpkgs-rust-1-75 {
+          inherit system;
+        };
       in pkgs.mkShell {
-        packages = with pkgs; [
+        packages = (with pkgs; [
           bashInteractiveFHS
           gnumake
-
+        ]) ++ (with rust1-75; [
+          rustc
           cargo
-        ];
+        ]);
 
         # Set an environment variable that will be read by the Makefile
         BUILD_MODE="DEBUG";
