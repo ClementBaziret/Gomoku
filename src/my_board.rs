76821d7aeb3e434fca3264458d6b2a5e12390b26
Phuitric <pierre.huitric@epitech.eu>
2024-11-05 17:54:52 +0100

    :wrench: Removed debug and all random implementations

--- src/my_board.rs
+++ src/my_board.rs
@@ -1,7 +1,3 @@
-// use rand::Rng;
-use std::fs::File;
-use std::io::Write;
-
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Status {
     Empty,
@@ -68,57 +64,9 @@ impl MyBoard {
         }
     }
 
-    // pub fn send_new_pos2(&mut self, mut file: &File) {
-    // //     send a random cell
-    //     let mut rng = rand::thread_rng();
-
-    //     let mut x: usize = rng.gen_range(0..=19);
-    //     let mut y: usize = rng.gen_range(0..=19);
-
-    //     let _ = file.write_all(
-    //         format!(
-    //             "checked value before: {}, {}, {}\n",
-    //             x,
-    //             y,
-    //             self.fetch_cell(x, y).to_str()
-    //         )
-    //         .as_bytes(),
-    //     );
-    //     while self.fetch_cell(x, y) != Status::Empty {
-    //         let _ = file.write_all(
-    //             format!("failed value: {}, {}\n", x, y).as_bytes(),
-    //         );
-
-    //         x = rng.gen_range(0..=19);
-    //         y = rng.gen_range(0..=19);
-    //     }
-    //     self.set_cell(x, y, Status::Ally);
-    //     let _ = file.write_all(
-    //         format!(
-    //             "checked value after : {}, {}, {}\n",
-    //             x,
-    //             y,
-    //             self.fetch_cell(x, y).to_str()
-    //         )
-    //         .as_bytes(),
-    //     );
-    //     println!("{}, {}", x, y);
-    // }
-
-    pub fn send_new_pos(&mut self, mut file: &File) {
-        // send a random cell
-
+    pub fn send_new_pos(&mut self) {
         let mut x: usize = 0;
         let mut y: usize = 0;
-        let _ = file.write_all(
-            format!(
-                "checked value before: {}, {}, {}\n",
-                x,
-                y,
-                self.fetch_cell(x, y).to_str()
-            )
-            .as_bytes(),
-        );
         while self.fetch_cell(x, y) != Status::Empty {
             x = x + 1;
             if x > self.size - 1 {
@@ -126,15 +74,6 @@ impl MyBoard {
                 y = y + 1;
             }
         }
-        let _ = file.write_all(
-            format!(
-                "checked value after : {}, {}, {}\n",
-                x,
-                y,
-                self.fetch_cell(x, y).to_str()
-            )
-            .as_bytes(),
-        );
         self.set_cell(x, y, Status::Ally);
         println!("{}, {}", x, y);
     }
