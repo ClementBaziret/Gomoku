857298442ee1f11455840202d4c9eca0e2d7340e
Florent Charpentier <florent.charpentier@epitech.eu>
2024-11-09 12:15:33 +0100

    :sparkles: Added Columns iterator

--- src/grid_iterators.rs
+++ src/grid_iterators.rs
@@ -21,8 +21,8 @@ impl<'a, T, const W: usize, const H: usize>
     }
 }
 
-impl<'a, T, const WIDTH: usize, const HEIGHT: usize> Iterator
-    for GridColumnIterator<'a, T, WIDTH, HEIGHT>
+impl<'a, T, const W: usize, const HEIGHT: usize> Iterator
+    for GridColumnIterator<'a, T, W, HEIGHT>
 {
     type Item = &'a T;
 
@@ -32,6 +32,41 @@ impl<'a, T, const WIDTH: usize, const HEIGHT: usize> Iterator
         } else {
             let ret = &self.inner_grid[self.index][self.selected_col];
             self.index += 1;
+
+            Some(ret)
+        }
+    }
+}
+
+impl<'a, T, const W: usize, const H: usize> GridColumns<'a, T, W, H> {
+    fn new(grid: &'a [[T; W]; H]) -> Self {
+        Self {
+            inner_grid: grid,
+            current_column: 0,
+        }
+    }
+}
+
+struct GridColumns<'a, T, const WIDTH: usize, const HEIGHT: usize> {
+    inner_grid: &'a [[T; WIDTH]; HEIGHT],
+    current_column: usize,
+}
+
+impl<'a, T, const WIDTH: usize, const H: usize> Iterator
+    for GridColumns<'a, T, WIDTH, H>
+{
+    type Item = GridColumnIterator<'a, T, WIDTH, H>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_column >= WIDTH {
+            None
+        } else {
+            let ret = GridColumnIterator::new(
+                self.inner_grid,
+                self.current_column,
+            );
+
+            self.current_column += 1;
 
             Some(ret)
         }
@@ -55,4 +90,18 @@ fn test_column_iterator() {
     assert_eq!(*col_it.next().unwrap(), 5);
     assert_eq!(*col_it.next().unwrap(), 6);
     assert_eq!(col_it.next(), None);
+}
+
+#[test]
+fn test_columns_iterator() {
+    let test_grid = [[1, 4, 7], [2, 5, 8], [3, 6, 9]];
+    let mut expected = 0;
+
+    for col in GridColumns::new(&test_grid) {
+        for val in col {
+            expected += 1;
+            assert_eq!(expected, *val);
+        }
+    }
+    assert_eq!(expected, 9);
 }
